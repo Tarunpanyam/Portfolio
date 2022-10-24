e29f2f31a46d82d1083f87c18d21b3a5ac48f297
Tarun panyam <51309831+Tarunpanyam@users.noreply.github.com>
2022-10-24 11:12:38 +0530

    Minor changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
             <BsWhatsapp className="contact__option-icon"/>
             <h4>WhatsApp</h4>
             <h5>+91 8639422619</h5>
-            <a href="https://api.whatsapp.com/send/?phone=8639422619" target="_blank">Send a message</a>
+            <a href="https://api.whatsapp.com/send/?phone=8639422619" target='_blank' >Send a message</a>
           </article>
 
           <article className="contact__option">
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import ReactDOM from "react-dom"
+// import ReactDOM from "react-dom"
 import App from "./App"
 import './index.css'
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App/>)
